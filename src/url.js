bcc8e74163f61bde336c6cbc8403d889ced637e6
R Japh <almandajr004@gmail.com>
2024-05-10 14:56:06 +0300

    first commit

---
+++ src/url.js
@@ -0,0 +1,38 @@
+const url = [
+  {
+    id: "rec6d6T3q5EBIdCfD",
+    name: "Best of Kitale in 7 Days Tour",
+    info: "Commencing your journey in the tranquil town of Kitale, nestled amidst the picturesque landscapes of Kenya's Rift Valley, you'll embark on a captivating 7-day expedition immersing yourself in the natural wonders and cultural delights of this charming destination. Guided by knowledgeable local experts, you'll explore the lush greenery of Saiwa Swamp National Park, encounter rare Sitatunga antelopes, and marvel at the breathtaking beauty of Mount Elgon. From enriching visits to local markets and cultural centers to exhilarating hikes through pristine forests, each day offers a myriad of unforgettable experiences. Join us for the Best of Kitale in 7 Days and uncover the hidden treasures of this enchanting town!",
+    image: "https://www.course-api.com/images/tours/tour-1.jpeg",
+    price: "1,995",
+  },
+  {
+    id: "recIwxrvU9HfJR3B4",
+    name: "Best of Nairobi in 14 Days Tour",
+    info: "Commencing your adventure in the bustling metropolis of Nairobi, you'll embark on a 10-day odyssey delving into the diverse landscapes and rich cultural heritage of Kenya's capital region. Guided by seasoned local guides, you'll traverse the rugged terrain of Aberdare National Park, witness the majesty of the Great Rift Valley, and encounter the unique wildlife of Lake Nakuru National Park. From exhilarating game drives to serene boat cruises on Lake Naivasha, each day promises a tapestry of unforgettable experiences. Join us for the Best of Nairobi in 10 Days and uncover the hidden treasures of Kenya's vibrant heart!",
+    image: "https://www.course-api.com/images/tours/tour-2.jpeg",
+    price: "3,895",
+  },
+  {
+    id: "recJLWcHScdUtI3ny",
+    name: "Best of Mombasa in 8 Days Tour",
+    info: "Beginning your journey in the bustling city of Mombasa, you'll embark on a 10-day exploration of the treasures awaiting in Kenya's coastal paradise. Guided by knowledgeable local experts, you'll uncover the historical wonders of Fort Jesus, immerse yourself in the vibrant culture of the Old Town, and bask in the sun-kissed beaches of Diani. From thrilling safaris in Tsavo National Park to tranquil boat rides in the serene waters of Wasini Island, each day offers a kaleidoscope of experiences. Join us for the Best of Mombasa in 10 Days and indulge in the splendor of Kenya's coastal gem!",
+    image: "https://www.course-api.com/images/tours/tour-3.jpeg",
+    price: "2,695",
+  },
+  {
+    id: "recK2AOoVhIHPLUwn",
+    name: "Best of Rome in 7 Days Tour",
+    info: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!",
+    image: "https://www.course-api.com/images/tours/tour-4.jpeg",
+    price: "2,095",
+  },
+  {
+    id: "receAEzz86KzW2gvH",
+    name: "Best of Nanyuki in 10 Days Tour",
+    info: "Starting in the vibrant city of Nanyuki, you'll embark on a 10-day adventure immersing yourself in the unparalleled beauty and rich cultural tapestry of this enchanting destination. Guided by seasoned local experts, you'll explore iconic landmarks such as Mount Kenya Safari Club, Ol Pejeta Conservancy, and Ngare Ndare Forest, encountering diverse wildlife and breathtaking landscapes along the way. From traversing the equator to engaging with local communities, each day promises new discoveries and unforgettable experiences. Join us for the Best of Nanyuki in 10 Days and uncover the hidden gems of this captivating town!",
+    image: "https://www.course-api.com/images/tours/tour-5.jpeg",
+    price: "2,595",
+  },
+];
+export default url;
